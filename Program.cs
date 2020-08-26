51b82c9914c3acc06bff6a068951a155674cacde
Talha Zahur <talha_zahur@hotmail.com>
2020-08-26 21:21:12 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _sevices;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _sevices = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "";
+
+            // event subscription
+            _client.Log += Log;
+
+            await RegisterCommandsAysnc();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAysnc()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix(">",ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+                var result = await _commands.ExecuteAsync(context, argPos, _sevices);
+
+                if (result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+
+               
+            }
+
+        }
+   
+    }
+}
---
+++ ping.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace bot
+{
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Pingasync()
+        {
+            await ReplyAsync("Hello World");
+        }
+
+        [Command("Your Private Message")]
+        public async Task tingasync()
+        {
+            await ReplyAsync("Any Response");
+        }
+
+        [Command("Where are you?")]
+        public async Task reply()
+        {
+            await ReplyAsync("I will not tell you");
+        }
+
+        [Command("kya kar rahay ho")]
+        public async Task reply1()
+        {
+            await ReplyAsync("mar wa raha");
+        }
+        [Command("tell me something")]
+        public async Task reply2()
+        {
+            await ReplyAsync("Please ask something");
+        }
+        [Command("whats your favourite colour?")]
+        public async Task reply3()
+        {
+            await ReplyAsync("yellow");
+        }
+
+        [Command("time")]
+        public async Task reply4()
+        {
+            await ReplyAsync(DateTime.Now.Day.ToString());
+        }
+        [Command("rate")]
+        public async Task reply5()
+        {
+            await ReplyAsync("Enter ammount in numbers!");
+            
+        }
+        
+        public double cal(double num)
+        {
+            double result;
+            result = num * 129.7;
+            return result; 
+        }
+        //[Command(R)]
+        //public async Task Say()
+        //{
+        //    double result;
+        //    result =  1* 129.7;
+            
+        //    // ReplyAsync is a method on ModuleBase
+        //    await ReplyAsync(result.ToString());
+            
+        //}
+
+    }
+}
